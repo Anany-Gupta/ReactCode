e1c137be2eedca556bb28f521326d2e51de5a16a
Anany-Gupta <ananygupta@gmail.com>
2021-08-04 20:26:10 +0530

    First

---
+++ practice/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import List from "./List"
+import Input from "./Input.jsx"
+class App extends React.Component {
+  state = {
+
+  };
+
+  render = () => {
+    
+  };
+}
+
+export default App;
